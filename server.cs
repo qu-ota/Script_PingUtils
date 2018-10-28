ab7b036c988fe45916eed720a9d62b25432d2b96
dom <34490428+qu-ota@users.noreply.github.com>
2018-10-28 15:11:00 -0400

    Update server.cs
    -- Moved function ping() to its own portion, changed previous area to simply ping();
    -- Added comments for code viewers

--- server.cs
+++ server.cs
@@ -1,25 +1,32 @@
-$PingScoringEnabled = false;
+/*
+Ping Utilities (Script_PingUtils)
+By Dominoes (37977), help from Blockland Content Creators ( https://discord.gg/r6ERkqy ), ping scoring script donated by ImPanda (30233)
+-----
+Comments next to many lines are for intrigued code-doers, if you don't like them safely ignore them
+*/
 
-function serverCmdTogglePingScore(%client)
+$PingScoringEnabled = false; //setting variable for the function
+
+function ping() //stating the initial ping function
 {
-	if(%client.isAdmin)
+	if(isEventPending($pingSchedule))
+		cancel($pingSchedule); //using this command in console disables the function, used later on
+
+	for(%i = 0; %i < clientGroup.GetCount(); %i++)
+		clientGroup.GetObject(%i).Score = clientGroup.GetObject(%i).GetPing(); //gets a user's score, then sets it to that user's ping
+
+	$pingSchedule = schedule(100, 0, ping); //schedules the function so it loops
+}
+
+function serverCmdTogglePingScore(%client) //server command to toggle above function
+{
+	if(%client.isAdmin) //checks if user is admin
 	{
-		if($PingScoringEnabled == false)
+		if($PingScoringEnabled == false) //checks if the variable at the top is set to false. notice the == instead of =
 		{
-			function ping()
-			{
-				if(isEventPending($pingSchedule))
-					cancel($pingSchedule);
-			
-				for(%i = 0; %i < clientGroup.GetCount(); %i++)
-					clientGroup.GetObject(%i).Score = clientGroup.GetObject(%i).GetPing();
-			
-				$pingSchedule = schedule(100, 0, ping);
-			}
-			
 			ping();
 			
-			$PingScoringEnabled = true;
+			$PingScoringEnabled = true; //modifies the variable above
 		
 			messageClient(%client,'',"\c6Ping score function \c2enabled\c6.");
 			messageall('MsgAdminForce', "\c2" @ %client.name SPC "\c6has enabled ping scores.");
@@ -42,10 +49,10 @@ function serverCmdGetPing(%client, %target)
 {
 	if(%client.isAdmin)
 	{
-		%target = findclientbyname(%target);
-		if(isObject(%target))
+		%target = findclientbyname(%target); //sets a variable, %target, to the object's name put in after the initial /getping
+		if(isObject(%target)) //determines if the target object is a player
 		{
-			%ping = %target.getPing();
+			%ping = %target.getPing(); //gets the target's ping, sets it to the variable stated
 			announce("\c2" @ %client.name SPC "\c6checked\c2" SPC %target.name @ "\c6's ping:\c5" SPC %ping @ "ms");
 		}
 		else
